27b0fc4ced7eedd1b29fe9f9f4c87767b8f34586
Austen Ito <austen.services@gmail.com>
2011-11-13 13:51:12 -1000

    win condition

--- js/connect_four.js
+++ js/connect_four.js
@@ -26,16 +26,64 @@ ConnectFour.prototype = {
      var pieces = this.get_pieces(row);
      pieces[column] = this.active_player;
      this.board_pieces[row] = pieces;
-     
-     if (this.active_player === this.players[0]) {
-       this.active_player = this.players[1];
-     }
-     else {
-       this.active_player = this.players[0];
-     }
   },
 
-  is_winning_move:  function(row, column) {
+  change_turn: function() {
+   if (this.active_player === this.players[0]) {
+     this.active_player = this.players[1];
+   }
+   else {
+     this.active_player = this.players[0];
+   }
+  },
+
+  is_winning_move:  function(row, column, player) {
+    var connect_matrix = [];
+    connect_matrix['up'] = [];
+    connect_matrix['up_right'] = [];
+    connect_matrix['right'] = [];
+    connect_matrix['down_right'] = [];
+    connect_matrix['down'] = [];
+    connect_matrix['down_left'] = [];
+    connect_matrix['left'] = [];
+    connect_matrix['up_left'] = [];
+
+    console.log("checking: " + row + ", " + column);
+
+    for (var i = 1; i < 4; i++) {
+      connect_matrix['up'].push([column, row - i]);
+      connect_matrix['up_right'].push([column + i, row - i]);
+      connect_matrix['right'].push([column + i, row]);
+      connect_matrix['down_right'].push([column + i, row + i]);
+      connect_matrix['down'].push([column, row + i]);
+      connect_matrix['down_left'].push([column - i, row + i]);
+      connect_matrix['left'].push([column - i, row]);
+      connect_matrix['up_left'].push([column - i, row - i]);
+    }
+
+    for (var matrix_key in connect_matrix) {
+      var cells = connect_matrix[matrix_key];
+      
+      console.log(matrix_key);
+      for (var i = 0; i < 3; i++) {
+        var cell = cells[i];
+        var column = cell[0];
+        var row = cell[1];
+        var pieces = this.get_pieces(row);
+        var piece = pieces[column];
+
+        console.log("player(" + player + "): " + row + ", " + column + ": " + piece);
+        if(!this.has_piece(row, column) || piece !== player || row < 0 
+          || column < 0 ) {
+          break;
+        }
+        else if(i === 2) {
+          return true;
+        }
+      }
+      console.log('====');
+    }
+    return false;
   },
 
   get_active_player: function() {
